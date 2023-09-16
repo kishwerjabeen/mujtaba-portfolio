a32d81df06e54b84bc69870a871aedfe2eb28a7e
kishwerjabeen <kishwerjabeen90@gmail.com>
2023-09-16 16:48:54 +0500

    nav image done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './components/nav'
+import TopLeftImg from './components/TopLeftImg'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <TopLeftImg/>
         <Navbar/>
         {children}</body>
     </html>
