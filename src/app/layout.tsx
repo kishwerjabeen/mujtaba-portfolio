1fb2b5eeda5d136902cca05b3b5e8118f01fd341
kishwerjabeen <kishwerjabeen90@gmail.com>
2023-09-18 15:51:09 +0500

    font integrate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,16 @@ import { Inter } from 'next/font/google'
 import Navbar from './components/nav'
 import TopLeftImg from './components/TopLeftImg'
 
+// fonts
+import { Sora } from "next/font/google"; '@next/font/google'
+
+//  font assing
+const sora = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800']
+});
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,7 +28,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`page bg-site text-white bg-cover bg-no-repeat `}>
+      <body className={"page bg-site text-white bg-cover bg-no-repeat ${sora.vaiable} font-sora relative "}>
         <TopLeftImg/>
         <Navbar/>
         {children}</body>
